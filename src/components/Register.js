d85fd1f295d2cabed2513776d066917a92358f00
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-08 13:52:08 -0300

    register service

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,10 @@
 import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import axios from './api/axios';
+import register from "../services/register";
 
 const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
 const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
-const REGISTER_URL = '/register';
-
 const Register = () => {
     const userRef = useRef();
     const errRef = useRef();
@@ -52,33 +50,24 @@ const Register = () => {
             setErrMsg("Invalid Entry");
             return;
         }
-        try {
-            const response = await axios.post(REGISTER_URL,
-                JSON.stringify({ user, pwd }),
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
-                }
-            );
-            console.log(response?.data);
-            console.log(response?.accessToken);
-            console.log(JSON.stringify(response))
+        const response = await register({user,pwd})
+        console.log(response);
+        console.log(response.message)
+        if (response?.errorCode === 500) {
+            setErrMsg('No Server Response');
+        } else if (response?.errorCode === 409) {
+            setErrMsg('Username Taken');
+        } else {
+            setErrMsg('Registration Failed')
+        }
+        if(response.data){
+            errRef.current.focus();
             setSuccess(true);
-            //clear state and controlled inputs
-            //need value attrib on inputs for this
             setUser('');
             setPwd('');
             setMatchPwd('');
-        } catch (err) {
-            if (!err?.response) {
-                setErrMsg('No Server Response');
-            } else if (err.response?.status === 409) {
-                setErrMsg('Username Taken');
-            } else {
-                setErrMsg('Registration Failed')
-            }
-            errRef.current.focus();
         }
+            
     }
 
     return (
---
+++ src/services/register.js
@@ -0,0 +1,20 @@
+import axios from '../api/axios';
+const REGISTER_URL = '/register';
+const register = async ({user,pwd}) => {
+
+  try {
+    const response = await axios.post(REGISTER_URL,
+        JSON.stringify({ user, password:pwd }),
+        {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+        }
+    );
+    return response;
+  }
+  catch (err) {
+    return {errorCode:parseInt(err?.message?.split(' ').splice(-1)[0])? parseInt(err?.message?.split(' ').splice(-1)[0]) : 500 }
+  }
+}
+
+export default register;
