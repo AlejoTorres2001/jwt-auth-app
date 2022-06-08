9f89a5873663822b066f5cfe11e1e74f6bc1e890
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-08 20:52:37 -0300

    login component + service

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,8 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect, useState, useContext } from "react";
+import AuthContext from "../context/AuthProvider";
+import login from "../services/login";
 const Login = () => {
+  const { auth, setAuth } = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
 
@@ -16,62 +19,76 @@ const Login = () => {
   }, [user, pwd]);
   const handleSubmit = async (e) => {
     e.preventDefault();
-
-  }
+    const response = await login({ user, pwd });
+    if (response.errorCode) errRef.current.focus();
+    if (response?.errorCode === 401) return setErrMsg("Invalid Credentials");
+    if (response?.errorCode === 400) return setErrMsg("Missing Username or Password");
+    if (response?.errorCode === 500) return setErrMsg("No Server Response");
+    //response success
+    const accessToken = response?.data?.accessToken;
+    const roles = response?.data?.roles;
+    if (accessToken) {
+      setAuth({ user, pwd, roles, accessToken });
+      setSuccess(true);
+      setUser("");
+      setPwd("");
+    }
+  };
   return (
     <>
-    { success ? (
-      <section>
+      {success ? (
+        <section>
           <h1>You are logged in!</h1>
           <br />
           <p>
-              <a href="#">Go to Home</a>
+            <a href="#">Go to Home</a>
           </p>
-      </section>
-  ) :(
-    <section>
-    <p
-      ref={errRef}
-      className={errMsg ? "errmsg" : "offscreen"}
-      aria-live="assertive"
-    >
-      {errMsg}
-    </p>
-    <h1>SignIn</h1>
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="username">Username</label>
-      <input
-        type="text"
-        id="username"
-        ref={userRef}
-        autoComplete="off"
-        onChange={(e) => setUser(e.target.value)}
-        value={user}
-        required
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        autoComplete="off"
-        onChange={(e) => setPwd(e.target.value)}
-        value={pwd}
-        required
-      />
-      <button>SigIn</button>
-    </form>
-    <p>
-      Need an Account?
-      <br />
-      <span className="line">
-        {/*put router link here*/}
-        <a href="#">Sign Up</a>
-      </span>
-    </p>
-  </section>
-  )}
-  </>
-  )
+        </section>
+      ) : (
+        <section>
+          <p
+            ref={errRef}
+            className={errMsg ? "errmsg" : "offscreen"}
+            aria-live="assertive"
+          >
+            {errMsg}
+          </p>
+          <h1>SignIn</h1>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text"
+              id="username"
+              ref={userRef}
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              autoComplete="off"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required
+              
+            />
+            <button>SigIn</button>
+          </form>
+          <p>
+            Need an Account?
+            <br />
+            <span className="line">
+              {/*put router link here*/}
+              <a href="#">Sign Up</a>
+            </span>
+          </p>
+        </section>
+      )}
+    </>
+  );
 };
 
 export default Login;
---
+++ src/services/login.js
@@ -0,0 +1,22 @@
+import axios from "../api/axios";
+
+const LOGIN_URL ='/auth';
+
+const login = async ({user,pwd}) => {
+
+  try {
+    const response = await axios.post(LOGIN_URL,
+        JSON.stringify({ user, password:pwd }),
+        {
+            headers: { 'Content-Type': 'application/json' },
+            withCredentials: true
+        }
+    );
+    return response;
+  }
+  catch (err) {
+    return {errorCode:parseInt(err?.message?.split(' ').splice(-1)[0])? parseInt(err?.message?.split(' ').splice(-1)[0]) : 500 }
+  }
+}
+
+export default login 
