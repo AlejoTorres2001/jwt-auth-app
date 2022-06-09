1bad3a9371e510f03daead1cb37c8dde3e45c27a
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-08 21:37:11 -0300

    setting up react router

--- src/index.js
+++ src/index.js
@@ -3,12 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AuthProvider } from './context/AuthProvider';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
+    
     <AuthProvider>
-    <App />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </AuthProvider>
+    
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
