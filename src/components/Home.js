388b9595a217d9f08543afb3e8f3fa551e252e30
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-09 17:23:24 -0300

    logout function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import useAuth from '../hooks/useAuth';
 
 const Home = () => {
+  const { setAuth } = useAuth();
+  const navigate = useNavigate()
+  const logout = async () => {
+    // axios to /logout endpoint 
+    setAuth({});
+    navigate('/login', { replace: true });
+}
   return (
-    <div>esto es Home</div>
+    <section>
+    <h1>Home</h1>
+    <br />
+    <p>You are logged in!</p>
+    <br />
+    <Link to="/editor">Go to the Editor page</Link>
+    <br />
+    <Link to="/admin">Go to the Admin page</Link>
+    <br />
+    <Link to="/lounge">Go to the Lounge</Link>
+    <br />
+    <Link to="/linkpage">Go to the link page</Link>
+    <div className="flexGrow">
+        <button onClick={()=>logout()}>Sign Out</button>
+    </div>
+</section>
   )
 }
 
