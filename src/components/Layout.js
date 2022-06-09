10971c684f98f1edb7ade07a94748f92b8892812
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-08 22:07:12 -0300

    home and layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
-    <main className='App'>
-
-      <Outlet></Outlet>
+    <main className="App">
+      <Outlet />
     </main>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
