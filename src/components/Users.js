7f875ca6db12b0f12fa635886817bfacba7160dc
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-10 17:16:28 -0300

    custom users hook

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,33 +1,6 @@
-import { useState, useEffect } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
-import useAxiosPrivate from "../hooks/useAxiosPrivate";
-
+import useUsers from "../hooks/useUsers";
 const Users = () => {
-  const [users, setUsers] = useState([]);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const axiosPrivate = useAxiosPrivate();
-  useEffect(() => {
-    let isMounted = true;
-    const controller = new AbortController();
-
-    const getUsers = async () => {
-      try {
-        const response = await axiosPrivate.get("/users", {
-          signal: controller.signal,
-        });
-        isMounted && setUsers(response.data);
-      } catch (error) {
-        console.error(error);
-        navigate("/login", { state: { from: location }, replace: true });
-      }
-    };
-    getUsers();
-    return () => {
-      isMounted = false;
-      controller.abort();
-    };
-  }, []);
+  const [users] = useUsers();
   return (
     <article>
       <h2>Users Lists</h2>
---
+++ src/hooks/useUsers.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import useAxiosPrivate from "./useAxiosPrivate";
+
+const useUsers = () => {
+  const navigate = useNavigate();
+  const axiosPrivate = useAxiosPrivate();
+  const [users, setUsers] = useState([]);
+  const location = useLocation();
+  useEffect(() => {
+    let isMounted = true;
+    const controller = new AbortController();
+
+    const getUsers = async () => {
+      try {
+        const response = await axiosPrivate.get("/users", {
+          signal: controller.signal,
+        });
+        isMounted && setUsers(response.data);
+      } catch (error) {
+        console.error(error);
+        navigate("/login", { state: { from: location }, replace: true });
+      }
+    };
+
+    getUsers();
+    return () => {
+      isMounted = false;
+      controller.abort();
+    };
+  }, []);
+  return [users, setUsers];
+};
+
+export default useUsers;
