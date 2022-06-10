80fe94bb9ed3dfcb7fdac6b63681843345bde021
AlejoTorres2001 <torres.alejo2001@gmail.com>
2022-06-10 17:16:19 -0300

    fixed hook

--- src/hooks/useToggle.js
+++ src/hooks/useToggle.js
@@ -4,7 +4,7 @@ const useToggle = (key, initValue) => {
   const [value, setValue] = useLocalStorage("persist", initValue);
   const toggle = () =>
     setValue((prev) => {
-      return typeof prev === "boolean" ? !value : !prev;
+      return typeof prev === "boolean" ? !value : prev;
     });
   return [value, toggle];
 };
